f5110a6caaec1e0f0476b3707b5962aa8872a11d
Joshua Richey <joshua.richey@aclimalabs.com>
2021-08-10 16:14:55 -0700

    copy updates

--- src/containers/about.js
+++ src/containers/about.js
@@ -25,11 +25,11 @@ export default () => [
     />
     </div>
     <section className="bio">
-      <p>Joshua Richey is a product strategist, user experience designer, and front-end developer with > 14 years of experience conceiving, researching, designing, and building digital tools for human use. He currently helps Aclima build interfaces that visualize air pollution at a hyperlocal level.</p>
-      <p>Motivated by change-making and world-saving, he previously led design at Idealist.org (an international jobs board for nonprofits), where he spearheaded human-centered design efforts, instituted a scalable design system, and rewrote the front end using htlm5, sass + postCSS, and react.</p>
-      <p>Prior, he ran a small consultancy where he led a team of designers and developers through the design and implemmentation of brand, print, and interactive experiences. He has designed products for Pink Floyd, Skydance Entertainment, and the Grand Canyon.</p>
+      <p>Joshua Richey is a product strategist, user experience designer, and front-end developer with > 20 years conceiving, researching, designing, and building digital tools for human use. He has designed products for Pink Floyd, Skydance Entertainment, and the Grand Canyon. Currently he helps Aclima build interfaces that visualize air pollution at a hyperlocal level.</p>
+      <p>Motivated by change-making and world-saving, he led design at Idealist.org (an international jobs board for nonprofits), where he spearheaded human-centered design efforts, instituted a scalable design system, and rewrote the front end using htlm5, sass + postCSS, and react.</p>
+      <p>Prior, he operated his own consultancy where he led a team of designers and developers through the design and implementation of brand, print, and interactive experiences.</p>
       <p>When not in front of his screen, he’s probably out walking around with his dog. When in front of the screen, he probably looks like this:</p>
-      <div className="working-pic-wrapper">
+        <div className="working-pic-wrapper">
         <ImgLoad
           url="jr-xix/working"
           ratio='1x1'
